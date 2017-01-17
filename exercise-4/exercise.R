50622e9ddf983d9f43e4aa25dcdacce32d5aa613
simran bhatia <simran18@uw.edu>
2017-01-17 12:03:32 -0800

    Merge remote-tracking branch 'info201-w17/master'
    # Conflicts:
    #	exercise-3/exercise.R

---
+++ exercise-4/exercise.R
@@ -0,0 +1,31 @@
+# Exercise 4: Creating and operating on vectors
+
+# Create a vector `names` that contains your name and the names of 2 people next to you.
+
+
+# Use the colon operator : to create a vector `n` of numbers from 10:49
+
+
+# Use `length()` to get the number of elements in `n`
+
+
+# Create a vector `m` that contains the numbers 10 to 1. Hint: use the `seq()` function
+
+
+# Subtract `m` FROM `n`. Note the recycling!
+
+
+# Use the `seq()` function to produce a range of numbers from -5 to 10 in `.1` increments.
+# Store it in a variable `x`
+
+
+# Create a vector `sin.wave` by calling the `sin()` function on each element in `x`.
+
+
+# Create a vector `cos.wave` by calling the `cos()` function on each element in `x`.
+
+
+# Create a vector `wave` by multiplying `sin.wave` and `cos.wave` together, then adding `sin.wave`
+
+
+# Use the `plot()` function to plot your `wave`!
